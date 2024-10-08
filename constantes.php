86ceee7f8b4722007d7d5f5e4712e523d0edc5f3
AndresGamboa11 <erickcarman98@gmail.com>
2024-10-07 20:51:52 -0500

    constantes php

---
+++ constantes.php
@@ -0,0 +1,10 @@
+<?php
+    /*crea un programa para convertir de cm A pulgada y de kg a libras */
+    define('PULGADAS',  0.393701);
+    define('LIBRAS',  2.20462);
+    $cm = 50;
+    $kg = 5;
+    echo  "cm a pulgada: " . $cm * PULGADAS ."\n";
+    echo   "kg a libras: " . $kg * LIBRAS ."\n";
+    
+?>
---
+++ operaciones.php
@@ -0,0 +1,12 @@
+<?php
+    //definir variables numero
+    $numero1=10;
+    $numero2=6;
+    echo "suma:" .  $numero1 + $numero2. "\n";
+    echo "resta:" .  $numero1 - $numero2. "\n";
+    echo  "multiplicacion:" .  $numero1 * $numero2. "\n";
+    echo  "division:" .  $numero1 / $numero2. "\n";
+    echo  "modulo:" .  $numero1 % $numero2. "\n";
+    echo  "exponente:" .  $numero1 ** $numero2. "\n";
+
+?>
